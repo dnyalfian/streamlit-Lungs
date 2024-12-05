9a27eb28232808385e12686e4492f11afb2776ce
dnyalfian <deny.al91@gmail.com>
2024-12-05 18:01:45 +0700

    Add files via upload

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -2,9 +2,8 @@ import streamlit as st
 from web import predict, load_data
 
 def app(df, x, y):
-    st.title('Cancer Risk Prediction Models')
+    st.title('Lungs Risk Prediction Models')
 
-    # Load data and display first few rows
     df, x, y = load_data()
 
     # Select model
@@ -13,39 +12,36 @@ def app(df, x, y):
     model_type_map = {"Decision Tree": "decision_tree", "Naive Bayes": "naive_bayes", "SVM": "svm"}
     model_type = model_type_map[model_choice]
 
-    # Prediction feature input
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        Age = st.number_input('Age', min_value=0)  # Set min_value to 0
-        Gender = st.number_input('Gender', min_value=0)  # Set min_value to 0
-        Air_Pollution = st.number_input('Air Pollution', min_value=0)  # Set min_value to 0
-        Alcohol_use = st.number_input('Alcohol use', min_value=0)  # Set min_value to 0
-        Dust_Allergy = st.number_input('Dust Allergy', min_value=0)  # Set min_value to 0
-        Occupational_Hazards = st.number_input('Occupational Hazards', min_value=0)  # Set min_value to 0
-        Genetic_Risk = st.number_input('Genetic Risk', min_value=0)  # Set min_value to 0
-        Chronic_Lung_Disease = st.number_input('Chronic Lung Disease', min_value=0)  # Set min_value to 0
+        Age = st.number_input('Age', min_value=0)  
+        Gender = st.number_input('Gender', min_value=0)  
+        Air_Pollution = st.number_input('Air Pollution', min_value=0)  
+        Alcohol_use = st.number_input('Alcohol use', min_value=0)  
+        Dust_Allergy = st.number_input('Dust Allergy', min_value=0)  
+        Occupational_Hazards = st.number_input('Occupational Hazards', min_value=0)  
+        Genetic_Risk = st.number_input('Genetic Risk', min_value=0)  
+        Chronic_Lung_Disease = st.number_input('Chronic Lung Disease', min_value=0)  
 
-    # Column 2: Lifestyle factors
     with col2:
-        Balanced_Diet = st.number_input('Balanced Diet', min_value=0)  # Set min_value to 0
-        Obesity = st.number_input('Obesity', min_value=0)  # Set min_value to 0
-        Smoking = st.number_input('Smoking', min_value=0)  # Set min_value to 0
-        Passive_Smoker = st.number_input('Passive Smoker', min_value=0)  # Set min_value to 0
-        Chest_Pain = st.number_input('Chest Pain', min_value=0)  # Set min_value to 0
-        Coughing_of_Blood = st.number_input('Coughing of Blood', min_value=0)  # Set min_value to 0
-        Fatigue = st.number_input('Fatigue', min_value=0)  # Set min_value to 0
-        Weight_Loss = st.number_input('Weight Loss', min_value=0)  # Set min_value to 0
+        Balanced_Diet = st.number_input('Balanced Diet', min_value=0)  
+        Obesity = st.number_input('Obesity', min_value=0)  
+        Smoking = st.number_input('Smoking', min_value=0)  
+        Passive_Smoker = st.number_input('Passive Smoker', min_value=0)  
+        Chest_Pain = st.number_input('Chest Pain', min_value=0)  
+        Coughing_of_Blood = st.number_input('Coughing of Blood', min_value=0)  
+        Fatigue = st.number_input('Fatigue', min_value=0)  
+        Weight_Loss = st.number_input('Weight Loss', min_value=0)  
 
-    # Column 3: Symptoms and signs
     with col3:
-        Shortness_of_Breath = st.number_input('Shortness of Breath', min_value=0)  # Set min_value to 0
-        Wheezing = st.number_input('Wheezing', min_value=0)  # Set min_value to 0
-        Swallowing_Difficulty = st.number_input('Swallowing Difficulty', min_value=0)  # Set min_value to 0
-        Clubbing_of_Finger_Nails = st.number_input('Clubbing of Finger Nails', min_value=0)  # Set min_value to 0
-        Frequent_Cold = st.number_input('Frequent Cold', min_value=0)  # Set min_value to 0
-        Dry_Cough = st.number_input('Dry Cough', min_value=0)  # Set min_value to 0
-        Snoring = st.number_input('Snoring', min_value=0)  # Set min_value to 0
+        Shortness_of_Breath = st.number_input('Shortness of Breath', min_value=0)  
+        Wheezing = st.number_input('Wheezing', min_value=0)  
+        Swallowing_Difficulty = st.number_input('Swallowing Difficulty', min_value=0)  
+        Clubbing_of_Finger_Nails = st.number_input('Clubbing of Finger Nails', min_value=0)  
+        Frequent_Cold = st.number_input('Frequent Cold', min_value=0)  
+        Dry_Cough = st.number_input('Dry Cough', min_value=0)  
+        Snoring = st.number_input('Snoring', min_value=0)  
 
     features = [Age, Gender, Air_Pollution, Alcohol_use, Dust_Allergy, Occupational_Hazards, Genetic_Risk, Chronic_Lung_Disease, 
                 Balanced_Diet, Obesity, Smoking, Passive_Smoker, Chest_Pain, Coughing_of_Blood, Fatigue, Weight_Loss, 
